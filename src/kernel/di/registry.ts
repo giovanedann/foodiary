d2e4051d979111c480369ae34d82741fdebdd5a6
Giovane Daniel <giovanedaniell@gmail.com>
2025-07-23 20:41:46 -0300

    feat(kernel/di): add Registry

---
+++ src/kernel/di/registry.ts
@@ -0,0 +1,37 @@
+type Constructor<T = any> = new (...args: any) => T;
+
+export class Registry {
+  private readonly providers = new Map<string, Registry.Provider>();
+
+  register(implementation: Constructor){
+    const token = implementation.name;
+
+    if (this.providers.has(token)) {
+      throw new Error(`${token} is already registered in registry.`);
+    }
+
+    const dependencies = Reflect.getMetadata("design:paramtypes", implementation) ?? [];
+
+    this.providers.set(token, { implementation, dependencies });
+  }
+
+  resolve<TImplementation extends Constructor>(implementation: TImplementation): InstanceType<TImplementation> {
+    const token = implementation.name;
+    const provider = this.providers.get(token);
+
+    if (!provider) {
+      throw new Error(`${token} is not registered in registry.`);
+    }
+
+    const dependencies = provider.dependencies.map(dep => this.resolve(dep));
+
+    return new provider.implementation(...dependencies);
+  }
+}
+
+export namespace Registry {
+  export type Provider = {
+    implementation: Constructor;
+    dependencies: Constructor[];
+  }
+}
