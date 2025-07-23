fc7d0440769a29443068b03f8b4fa847c7049c74
Giovane Daniel <giovanedaniell@gmail.com>
2025-07-23 20:54:43 -0300

    fix: decorator not emitting metadata due to esbuild non-typechecking behavior

--- src/main/functions/hello.ts
+++ src/main/functions/hello.ts
@@ -5,6 +5,13 @@ import { HelloUseCase } from "@application/use-cases/hello.usecase";
 
 import { lambdaHttpAdapter } from "@main/adapters/lambda-http-adapter";
 
-const controller = new HelloController(new HelloUseCase());
+import { Registry } from "@kernel/di/registry";
+
+const container = new Registry();
+
+container.register(HelloUseCase);
+container.register(HelloController);
+
+const controller = container.resolve(HelloController);
 
 export const handler = lambdaHttpAdapter(controller);
