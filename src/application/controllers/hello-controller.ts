cfabf2db4e5380c1ccbdd221f2e971ddecc08712
Giovane Daniel <giovanedaniell@gmail.com>
2025-07-23 17:59:51 -0300

    feat: add schema folder to controller folder

--- src/application/controllers/hello-controller.ts
+++ src/application/controllers/hello-controller.ts
@@ -1,18 +1,11 @@
-import { z } from "zod";
 import { Controller } from "../contracts/controller";
-
-const schema = z.object({
-  name: z.string().min(1, { error: "Name is required" }),
-  email: z
-    .email({ error: "Email is invalid" })
-    .min(1, { error: "Email is required" }),
-});
+import { HelloBody, helloSchema } from "./schemas/hello-schema";
 
 export class HelloController extends Controller<unknown> {
-  protected override schema = schema;
+  protected override schema = helloSchema;
 
   protected override async handle(
-    request: Controller.Request,
+    request: Controller.Request<HelloBody>,
   ): Promise<Controller.Response<unknown>> {
     return {
       statusCode: 200,
