18dad3aad67ab932c1cb460e430c0b9d01ac90dd
Giovane Daniel <giovanedaniell@gmail.com>
2025-07-29 23:45:26 -0300

    chore: remove unnecessary file

--- src/kernel/di/container.ts
+++
@@ -1,8 +0,0 @@
-import { HelloController } from "@application/controllers/hello.controller";
-import { HelloUseCase } from "@application/use-cases/hello.usecase";
-import { Registry } from "@kernel/di/registry";
-
-export const container = Registry.getInstance();
-
-container.register(HelloUseCase);
-container.register(HelloController);
