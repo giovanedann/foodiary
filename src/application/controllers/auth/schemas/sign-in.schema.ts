0403a79de186239c83d94df281edd05480bf9dbe
Giovane Daniel <giovanedaniell@gmail.com>
2025-08-01 18:03:20 -0300

    feat: improve error messages for schemas

--- src/application/controllers/auth/schemas/sign-in.schema.ts
+++ src/application/controllers/auth/schemas/sign-in.schema.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 export const signInSchema = z.object({
   email: z
-    .email({ error: "Email is invalid" })
-    .min(1, { error: "Email is required" }),
-  password: z.string().min(1, { error: "Password  is required" }),
+    .email({ error: "field 'email' has an invalid format" })
+    .min(1, { error: "field 'email' is required on the request body" }),
+  password: z
+    .string()
+    .min(1, { error: "field 'password' is required on the request body" }),
 });
 
 export type SignInBody = z.infer<typeof signInSchema>;
--- src/application/controllers/auth/schemas/sign-up.schema.ts
+++ src/application/controllers/auth/schemas/sign-up.schema.ts
@@ -2,15 +2,26 @@ import { z } from "zod";
 
 export const signUpSchema = z.object({
   account: z.object({
-    email: z.email({ error: "Email is invalid" }).min(1, { error: "Email is required" }),
+    email: z
+      .email({ error: "Field 'email' has an invalid format" })
+      .min(1, { error: "Field 'email' is required on the request body" }),
     password: z
       .string()
-      .min(8, { error: "Password should be at least 8 characters long" })
-      .regex(/[a-z]/, { error: "Password must contain at least one lowercase letter" })
-      .regex(/[A-Z]/, { error: "Password must contain at least one uppercase letter" })
-      .regex(/[0-9]/, { error: "Password must contain at least one number" })
-      .regex(/[\W]/, { error: "Password must contain at least one special character" })
-    ,
+      .min(8, {
+        error: "Field 'password' should be at least 8 characters long",
+      })
+      .regex(/[a-z]/, {
+        error: "Field 'password' must contain at least one lowercase letter",
+      })
+      .regex(/[A-Z]/, {
+        error: "Field 'password' must contain at least one uppercase letter",
+      })
+      .regex(/[0-9]/, {
+        error: "Field 'password' must contain at least one number",
+      })
+      .regex(/[\W]/, {
+        error: "Field 'password' must contain at least one special character",
+      }),
   }),
 });
 
