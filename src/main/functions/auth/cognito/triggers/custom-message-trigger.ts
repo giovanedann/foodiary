9d75b07cff7043aafe12b8a612d8079e988604c7
Giovane Daniel <giovanedaniell@gmail.com>
2025-08-02 11:05:59 -0300

    feat: add custom email template in custom message trigger

--- src/main/functions/auth/cognito/triggers/custom-message-trigger.ts
+++ src/main/functions/auth/cognito/triggers/custom-message-trigger.ts
@@ -1,11 +1,17 @@
+import ForgotPasswordEmail from "@infra/emails/templates/forgot-password";
+import { render } from "@react-email/render";
 import { CustomMessageTriggerEvent } from "aws-lambda";
 
 export async function handler(event: CustomMessageTriggerEvent) {
   if (event.triggerSource === "CustomMessage_ForgotPassword") {
     const recoveryCode = event.request.codeParameter;
 
+    const compiledEmail = await render(
+      ForgotPasswordEmail({ confirmationCode: recoveryCode })
+    );
+
     event.response.emailSubject = "🍏 Foodiary | Recupere sua conta";
-    event.response.emailMessage = `O seu código de recuperação é: ${recoveryCode}`;
+    event.response.emailMessage = compiledEmail;
   }
 
   return event;
