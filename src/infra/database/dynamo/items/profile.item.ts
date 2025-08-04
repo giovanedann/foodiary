95af2b90d4ea8cecba029b4e3c344d9630c10ce0
Giovane Daniel <giovanedaniell@gmail.com>
2025-08-04 16:32:48 -0300

    feat: add Profile item

---
+++ src/infra/database/dynamo/items/profile.item.ts
@@ -0,0 +1,75 @@
+import { Profile } from "@application/entities/profile.entity";
+import { AccountItem } from "./account.item";
+
+export class ProfileItem {
+  private readonly type = "Profile";
+  private readonly keys: ProfileItem.Keys;
+
+  constructor(private readonly attributes: ProfileItem.Attributes) {
+    this.keys = {
+      PK: ProfileItem.getPK(this.attributes.accountId),
+      SK: ProfileItem.getSK(this.attributes.accountId),
+    };
+  }
+
+  static fromEntity(profile: Profile): ProfileItem {
+    return new ProfileItem({
+      ...profile,
+      createdAt: profile.createdAt.toISOString(),
+      birthDate: profile.birthDate.toISOString(),
+    });
+  }
+
+  static toEntity(item: ProfileItem.ItemType): Profile {
+    return new Profile({
+      accountId: item.accountId,
+      name: item.name,
+      birthDate: new Date(item.birthDate),
+      gender: item.gender,
+      height: item.height,
+      weight: item.weight,
+      activityLevel: item.activityLevel,
+      createdAt: new Date(item.createdAt),
+    });
+  }
+
+  toDynamoItem(): ProfileItem.ItemType {
+    return {
+      ...this.keys,
+      ...this.attributes,
+      type: this.type,
+    };
+  }
+
+  static getPK(profileId: string): ProfileItem.Keys["PK"] {
+    return `ACCOUNT#${profileId}`;
+  }
+
+  static getSK(profileId: string): ProfileItem.Keys["SK"] {
+    return `ACCOUNT#${profileId}#PROFILE`;
+  }
+}
+
+export namespace ProfileItem {
+  export type Keys = {
+    PK: AccountItem.Keys["PK"];
+    SK: `ACCOUNT#${string}#PROFILE`;
+  };
+
+  export type Attributes = {
+    accountId: string;
+    name: string;
+    birthDate: string;
+    gender: Profile.Gender;
+    height: number;
+    weight: number;
+    activityLevel: Profile.ActivityLevel;
+    createdAt: string;
+  };
+
+  export type Type = {
+    type: "Profile";
+  };
+
+  export type ItemType = Keys & Attributes & Type;
+}
